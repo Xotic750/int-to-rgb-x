89f4ccc51aa0102210cf07d5a10aafc45851d476
Xotic750 <xotic750@gmail.com>
2017-09-03 19:58:03 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file Convert an integer to a RGB string.
- * @version 2.0.1
+ * @version 2.1.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
