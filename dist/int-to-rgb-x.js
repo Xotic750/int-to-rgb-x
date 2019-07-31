2e47c6f7abd70bf0d697526fb4863c2c06f3a0af
Graham Fairweather <xotic750@gmail.com>
2019-07-31 19:47:22 +0200

    Package rework

--- dist/int-to-rgb-x.js
+++ dist/int-to-rgb-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-31T16:17:25.846Z",
+  "date": "2019-07-31T17:47:15.836Z",
   "describe": "",
   "description": "Convert an integer to a RGB string.",
   "file": "int-to-rgb-x.js",
